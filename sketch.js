3a23dace340a65899c83df8ad3f352bf605d5911
Lion67lover <87661428+Lion67lover@users.noreply.github.com>
2021-07-30 11:23:43 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,43 +1,88 @@
-var lion
-function setup() {
-  createCanvas(400,400);
-   lion = createSprite(200,200,130,50);
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage,cloudImage;
+
+
+
+
+var score;
+
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
+  trex_collided = loadImage("trex_collided.png");
+  cloudImage = loadImage("cloud.png");
+  groundImage = loadImage("ground2.png");
+  
+ 
+  
 }
 
+function setup() {
 
-function draw() 
-{
-  background(30);
-  if(keyDown("right")){
-    lion.x = lion.x + 2;
+  createCanvas(600,200)
+  
+  //create a trex sprite
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  
+  //create a ground sprite
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  //creating invisible ground
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  //generate random numbers
+  var rand =  Math.round(random(1,100))
+  console.log(rand)
+
+}
+
+function draw() {
+  //set background color
+  background(180);
+  
+  console.log(trex.y)
+  
+  
+  
+  // jump when the space key is pressed
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
   }
-  if(keyDown("left")){
-    lion.x = lion.x - 2;
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
   }
-  if(keyDown("Up")){
-    lion.y = lion.y - 2; 
-  }
-  if(keyDown("Down")){
-    lion.y = lion.y + 2;
-  }
-  if(keyDown("R")){
-    background("red");
-  }
-  if(keyDown("B")){
-    background("blue");
-  }
-  if(keyDown("G")){
-    background("green");
-  }
-  if(keyDown("Y")){
-    background("yellow");
-  }
-  if(keyDown("P")){
-    background("pink");
-  }
+  
+  //stop trex from falling down
+  trex.collide(invisibleGround);
+  console.log(frameCount);
+  //Spawn Clouds
+  spawnClouds()
+  
   drawSprites();
 }
 
+//function to spawn the clouds
+function spawnClouds(){
+ // write your code here 
+  
+  if(frameCount%60===0){
+  var clouds = createSprite(700,88,30,45);
+  clouds.addImage(cloudImage);
+  clouds.y = Math.round(random(10,80));
+  clouds.velocityX = -4;
+  clouds.depth = trex.depth;
+  trex.depth = trex.depth + 1;
+  }
+}
 
 
 
